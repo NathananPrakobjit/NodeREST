ef551d66e7da9bcb10bdf5c95665c0d3536cac9d
NathananPrakobjit <64060216200255@fitm.kmutnb.ac.th>
2023-02-15 11:29:04 +0700

    update

--- CRUDBookNoDB.js
+++ CRUDBookNoDB.js
@@ -36,7 +36,7 @@ app.get('/books', (req, res) => {
 // route to get a book by id
 app.get('/books/:id', (req, res) => {
     const book = books.find(b => b.id == parseInt(req.params.id));
-    if (!book) res.status(404).send('book not found');
+    if (!book) res.status(404).send('Book not found');
     res.json(book);
 });
 
